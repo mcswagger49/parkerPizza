18d0c97621979fe87e10714f00a14d8a5f21fce7
Joshua Smith <joshsmith0419@gmail.com>
2021-03-11 12:39:01 -0500

    Delete pizzaFunctions.js

--- pizzaFunctions.js
+++
@@ -1,8 +0,0 @@
-// Change background to red
-function setRed() {
-pageTitle.style.background = "red";
-}
-// Change background to white
-function setWhite() {
-pageTitle.style.background = "white";
-}
