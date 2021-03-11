9b2775ff4aa8cb456048c503d9f363ddc103416d
Joshua Smith <joshsmith0419@gmail.com>
2021-03-11 12:40:24 -0500

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,8 @@
+// Change background to red
+function setRed() {
+pageTitle.style.background = "red";
+}
+// Change background to white
+function setWhite() {
+pageTitle.style.background = "white";
+}
